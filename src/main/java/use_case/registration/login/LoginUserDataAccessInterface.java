0f674c7807f9dd5588ec11734b54660ae73f91d3
Huzaifa123451 <huzaifaahmer978@gmail.com>
2025-11-17 19:50:41 -0500

    fixed some bugs with the exception handling

--- src/main/java/use_case/registration/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/registration/login/LoginUserDataAccessInterface.java
@@ -1,6 +1,7 @@
 package use_case.registration.login;
 
 import entity.User;
+import use_case.DataAccessException;
 
 /**
  * DAO interface for the Login Use Case.
@@ -12,20 +13,20 @@ public interface LoginUserDataAccessInterface {
      * @param username the username to look for
      * @return true if a user with the given username exists; false otherwise
      */
-    boolean existsByName(String username);
+    boolean existsByName(String username) throws DataAccessException;
 
     /**
      * Saves the user.
      * @param user the user to save
      */
-    void save(User user);
+    void save(User user) throws DataAccessException;
 
     /**
      * Returns the user with the given username.
      * @param username the username to look up
      * @return the user with the given username
      */
-    User get(String username);
+    User get(String username) throws DataAccessException;
 
     void setCurrentUsername(String name);
 
