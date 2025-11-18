f99a04e17f6753f8b59a8f5abde4646f1df8ff33
Daniel Zhang <danielzhang@Daniels-MacBook-Pro-7.local>
2025-11-18 14:56:55 -0500

    Removed local login DAO logic

--- src/main/java/use_case/registration/login/LoginInteractor.java
+++ src/main/java/use_case/registration/login/LoginInteractor.java
@@ -61,26 +61,5 @@ public class LoginInteractor implements LoginInputBoundary {
             loginPresenter.prepareSuccessView(loginOutputData);
             return;
         }
-
-        /// Restructured below s.t. DAO does not require api key.
-//        if (!userDataAccessObject.existsByName(username)) {
-//            loginPresenter.prepareFailView(username + ": Account does not exist.");
-//        }
-//        else {
-//            final String pwd = userDataAccessObject.get(username).getPassword();
-//            if (!password.equals(pwd)) {
-//                loginPresenter.prepareFailView("Incorrect password for \"" + username + "\".");
-//            }
-//            else {
-//
-//                final User user = userDataAccessObject.get(loginInputData.getUsername());
-//
-//                userDataAccessObject.setCurrentUsername(username);
-//
-//                final LoginOutputData loginOutputData = new LoginOutputData(user.getUserName());
-//                loginPresenter.prepareSuccessView(loginOutputData);
-//            }
-//        }
-
     }
 }
