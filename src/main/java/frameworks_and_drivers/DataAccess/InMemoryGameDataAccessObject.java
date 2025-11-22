3dd476fa17a776b3d55a3a14059b68430b8edfed
mahirc27 <mahirasifchowdhury@gmail.com>
2025-11-22 01:07:31 -0500

    Commented out unused file

--- src/main/java/frameworks_and_drivers/DataAccess/InMemoryGameDataAccessObject.java
+++ src/main/java/frameworks_and_drivers/DataAccess/InMemoryGameDataAccessObject.java
@@ -1,24 +1,24 @@
-//Mahir
-package frameworks_and_drivers.DataAccess;
-
-import entity.MultiPlayerGame;
-import use_case.MultiPlayer.MultiPlayerAccessInterface;
-import use_case.MultiPlayer.submit_answer.SubmitAnswerDataAccessInterface;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class InMemoryGameDataAccessObject implements MultiPlayerAccessInterface, SubmitAnswerDataAccessInterface {
-    private final Map<String, MultiPlayerGame> games = new HashMap<>();
-
-    @Override
-    public void save(MultiPlayerGame game) {
-        games.put(game.getPlayerA().getUserName(), game);
-        games.put(game.getPlayerB().getUserName(), game);
-    }
-
-    @Override
-    public MultiPlayerGame getMultiPlayerGame(String username) {
-        return games.get(username);
-    }
-}
+////Mahir
+//package frameworks_and_drivers.DataAccess;
+//
+//import entity.MultiPlayerGame;
+//import use_case.MultiPlayer.MultiPlayerAccessInterface;
+//import use_case.MultiPlayer.submit_answer.SubmitAnswerDataAccessInterface;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class InMemoryGameDataAccessObject implements MultiPlayerAccessInterface, SubmitAnswerDataAccessInterface {
+//    private final Map<String, MultiPlayerGame> games = new HashMap<>();
+//
+//    @Override
+//    public void save(MultiPlayerGame game) {
+//        games.put(game.getPlayerA().getUserName(), game);
+//        games.put(game.getPlayerB().getUserName(), game);
+//    }
+//
+//    @Override
+//    public MultiPlayerGame getMultiPlayerGame(String username) {
+//        return games.get(username);
+//    }
+//}
