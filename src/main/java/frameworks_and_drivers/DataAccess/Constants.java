2bf9f2eec63548ab03d2718ee642ff53f3e25ece
Daniel Zhang <66927489+DanielZ818@users.noreply.github.com>
2025-11-29 16:07:47 -0500

    Checkstyle for constants

--- src/main/java/frameworks_and_drivers/DataAccess/Constants.java
+++ src/main/java/frameworks_and_drivers/DataAccess/Constants.java
@@ -1,3 +1,17 @@
+/*
+ * Package: frameworks_and_drivers.DataAccess
+ * Class: Constants
+ *
+ * Description:
+ *   This class defines application-wide constants used for HTTP responses
+ *   and API configuration, including status codes, error messages, and API URLs.
+ *
+ * Author: Daniel Zhang
+ *
+ * Notes:
+ *   All constants are declared as static final and should not be modified.
+ */
+
 package frameworks_and_drivers.DataAccess;
 
 public class Constants {
