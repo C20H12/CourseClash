cb8e81d17644e4879a448e9e99aeab286dfb7710
Daniel Zhang <66927489+DanielZ818@users.noreply.github.com>
2025-11-29 16:18:40 -0500

    checkstyle signupDAO

--- src/main/java/frameworks_and_drivers/DataAccess/SignupUserDataAccessObject.java
+++ src/main/java/frameworks_and_drivers/DataAccess/SignupUserDataAccessObject.java
@@ -1,38 +1,15 @@
 package frameworks_and_drivers.DataAccess;
 
-import entity.User;
-import org.json.JSONObject;
-import use_case.DataAccessException;
-import use_case.registration.signup.SignupUserDataAccessInterface;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import static frameworks_and_drivers.DataAccess.StaticMethods.makeApiRequest;
+import org.json.JSONObject;
+
+import entity.User;
+import use_case.DataAccessException;
+import use_case.registration.signup.SignupUserDataAccessInterface;
 
 public class SignupUserDataAccessObject implements SignupUserDataAccessInterface {
-
-//    @Override
-//    public boolean existsByName(String username) throws DataAccessException {
-//        final String method = "/api/exists-user";
-//        Map<String, String> params = Map.of("username", username);
-//        JSONObject response = makeApiRequest("GET", method, params, apiKey);
-//        return response.getBoolean("exists");
-//    }
-
-//    @Override
-//    public void save(User user) throws DataAccessException {
-//        final String method = "/api/update-user-data";
-//        Map<String, String> params = Map.of(
-//                "username", user.getUserName(),
-//                "level", String.valueOf(user.getLevel()),
-//                "points", String.valueOf(user.getExperiencePoints()),
-//                "answered", String.valueOf(user.getQuestionsAnswered()),
-//                "correct", String.valueOf(user.getQuestionsCorrect())
-//        );
-//        makeApiRequest("POST", method, params, apiKey);
-//    }
-
 
     @Override
     public String create(User user) throws DataAccessException {
@@ -45,7 +22,7 @@ public class SignupUserDataAccessObject implements SignupUserDataAccessInterface
         params.put("points", String.valueOf(user.getExperiencePoints()));
         params.put("answered", String.valueOf(user.getQuestionsAnswered()));
         params.put("correct", String.valueOf(user.getQuestionsCorrect()));
-        JSONObject response = makeApiRequest("GET", method, params, signupApiKey);
+        JSONObject response = StaticMethods.makeApiRequest("GET", method, params, signupApiKey);
         return response.getString("status_message");
     }
 }
