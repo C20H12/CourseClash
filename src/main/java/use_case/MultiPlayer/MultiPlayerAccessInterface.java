2b274c4375a567b4299b9c4aadb4b51d7d83b8c8
mahirc27 <mahirasifchowdhury@gmail.com>
2025-11-29 16:09:41 -0500

    comments comments comments

--- src/main/java/use_case/MultiPlayer/MultiPlayerAccessInterface.java
+++ src/main/java/use_case/MultiPlayer/MultiPlayerAccessInterface.java
@@ -8,7 +8,16 @@ import entity.DeckManagement.StudyDeck;
 import interface_adapter.user_session.UserSession;
 
 public interface MultiPlayerAccessInterface {
-  List<StudyDeck> getAllDecks();
 
-  UserSession getSession();
+    /**
+     * Retrieves a list of all available study decks from the data source.
+     * @return A list containing all loaded StudyDeck objects.
+     */
+    List<StudyDeck> getAllDecks();
+
+    /**
+     * Retrieves the current active user session.
+     * @return The UserSession object containing current user details.
+     */
+    UserSession getSession();
 }
