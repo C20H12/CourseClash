e35a103617ce87316067ff1e9ef1c767df9a1a54
mahirc27 <mahirasifchowdhury@gmail.com>
2025-11-29 16:08:27 -0500

    comments changed

--- src/main/java/use_case/MultiPlayer/MultiPlayerInputBoundary.java
+++ src/main/java/use_case/MultiPlayer/MultiPlayerInputBoundary.java
@@ -4,15 +4,41 @@ import entity.User;
 import entity.DeckManagement.StudyDeck;
 
 public interface MultiPlayerInputBoundary {
-  void advance();
 
-  void endGame();
+    /**
+     * Proceeds to the next card in the deck or ends the game if the deck is complete.
+     */
+    void advance();
 
-  void showAllDecks();
+    /**
+     * Terminates the current multiplayer session manually.
+     */
+    void endGame();
 
-  void startGame(StudyDeck selectedDeck, User host, User guest);
+    /**
+     * Retrieves and displays the list of available study decks.
+     */
+    void showAllDecks();
 
-  void chooseAnswer(String option, boolean host);
+    /**
+     * Initializes a new multiplayer game session with the specified settings.
+     * @param selectedDeck The deck chosen for the game.
+     * @param host The user acting as the host.
+     * @param guest The user acting as the guest.
+     */
+    void startGame(StudyDeck selectedDeck, User host, User guest);
 
-  void updateOtherPlayerScore(int score, boolean host);
+    /**
+     * Processes the user's answer selection for the current card.
+     * @param option The answer string selected by the player.
+     * @param host True if the answer is from the host, false if from the guest.
+     */
+    void chooseAnswer(String option, boolean host);
+
+    /**
+     * Updates the score for the opposing player to maintain synchronization.
+     * @param score The score to update.
+     * @param host True if the update is for the guest (context dependent), false otherwise.
+     */
+    void updateOtherPlayerScore(int score, boolean host);
 }
