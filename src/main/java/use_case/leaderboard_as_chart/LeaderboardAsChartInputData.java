8cc56430bd1ad021e3f9a5d6e58de7bf630d631d
Daniel Zhang <66927489+DanielZ818@users.noreply.github.com>
2025-11-29 16:34:38 -0500

    leaderboard as chart input data check style

--- src/main/java/use_case/leaderboard_as_chart/LeaderboardAsChartInputData.java
+++ src/main/java/use_case/leaderboard_as_chart/LeaderboardAsChartInputData.java
@@ -3,6 +3,7 @@ package use_case.leaderboard_as_chart;
 public class LeaderboardAsChartInputData {
     private final String requestType;
     private final int leaderboardEntryCount;
+
     public LeaderboardAsChartInputData(String requestType, int leaderboardEntryCount) {
         this.requestType = requestType;
         this.leaderboardEntryCount = leaderboardEntryCount;
