a63094b8196e8f46abb4d548867a44d83d5e9445
Daniel Zhang <66927489+DanielZ818@users.noreply.github.com>
2025-11-29 16:13:42 -0500

    remove static import in test DAO

--- src/main/java/frameworks_and_drivers/DataAccess/LoginSignupTestDataAccessObject.java
+++ src/main/java/frameworks_and_drivers/DataAccess/LoginSignupTestDataAccessObject.java
@@ -8,8 +8,6 @@ import org.json.JSONObject;
 import entity.User;
 import use_case.DataAccessException;
 
-import static frameworks_and_drivers.DataAccess.StaticMethods.makeApiRequest;
-
 public class LoginSignupTestDataAccessObject {
     /**
      * Deletes a test user from the system.
@@ -22,7 +20,7 @@ public class LoginSignupTestDataAccessObject {
         final String deleteApiKey = "delete-test-user-only";
         Map<String, String> params = new HashMap<>();
         params.put("username", String.valueOf(user.getUserName()));
-        JSONObject response = makeApiRequest("GET", method, params, deleteApiKey);
+        JSONObject response = StaticMethods.makeApiRequest("GET", method, params, deleteApiKey);
         response.getString("status_message");
     }
 
@@ -43,7 +41,7 @@ public class LoginSignupTestDataAccessObject {
         params.put("points", String.valueOf(user.getExperiencePoints()));
         params.put("answered", String.valueOf(user.getQuestionsAnswered()));
         params.put("correct", String.valueOf(user.getQuestionsCorrect()));
-        JSONObject response = makeApiRequest("GET", method, params, signupApiKey);
+        JSONObject response = StaticMethods.makeApiRequest("GET", method, params, signupApiKey);
         return response.getString("status_message");
     }
 }
