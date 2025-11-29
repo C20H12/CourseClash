28a41f1ae341e6f558c688d03cebb3fef7cd9828
Huzaifa123451 <huzaifaahmer978@gmail.com>
2025-11-29 15:49:51 -0500

    checkstyle fixes - for InpputData

--- src/main/java/use_case/SinglePlayer/SinglePlayerInputBoundary.java
+++ src/main/java/use_case/SinglePlayer/SinglePlayerInputBoundary.java
@@ -1,12 +1,15 @@
 // huzaifa - start game, end game, submit answer
+
 package use_case.SinglePlayer;
 
 import use_case.DataAccessException;
 
 public interface SinglePlayerInputBoundary {
+
     void startGame(SinglePlayerInputData inputData) throws DataAccessException;
     void submitAnswer(String answer) throws DataAccessException;
     void endGame() throws DataAccessException;
     void showAllDeckNames() throws DataAccessException;
 }
+
 
--- src/main/java/use_case/SinglePlayer/SinglePlayerInputData.java
+++ src/main/java/use_case/SinglePlayer/SinglePlayerInputData.java
@@ -1,8 +1,6 @@
 //huzaifa - player, deck, timer, shuffle, numQuestions
 package use_case.SinglePlayer;
 
-import entity.DeckManagement.StudyCard;
-import entity.DeckManagement.StudyDeck;
 import entity.User;
 
 public class SinglePlayerInputData {
@@ -21,10 +19,20 @@ public class SinglePlayerInputData {
         this.numQuestions = numQuestions;
     }
 
-    public User getPlayer() { return player; }
-    public String getDeckTitle() { return deckTitle; }
-    public int getTimerPerQuestion() { return timerPerQuestion; }
-    public boolean isShuffle() { return shuffle; }
-    public int getNumQuestions() { return numQuestions; }
+    public User getPlayer() {
+        return player;
+    }
+    public String getDeckTitle() {
+        return deckTitle;
+    }
+    public int getTimerPerQuestion() {
+        return timerPerQuestion;
+    }
+    public boolean isShuffle() {
+        return shuffle;
+    }
+    public int getNumQuestions() {
+        return numQuestions;
+    }
 }
 
