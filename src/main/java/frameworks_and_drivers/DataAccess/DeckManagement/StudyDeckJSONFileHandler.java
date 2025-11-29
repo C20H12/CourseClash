677f366000b2b59cd423895ef48755292ce3c690
404Haze <108695733+404Haze@users.noreply.github.com>
2025-11-28 21:49:44 -0500

    Refactored with Checkstyle file-handling classes for Study Deck Management.

--- src/main/java/frameworks_and_drivers/DataAccess/DeckManagement/StudyDeckJSONFileHandler.java
+++ src/main/java/frameworks_and_drivers/DataAccess/DeckManagement/StudyDeckJSONFileHandler.java
@@ -161,6 +161,7 @@ public class StudyDeckJSONFileHandler {
         return json.toString();
     }
 
+
     // Convert JSON string back to StudyDeck object
     // @param json The JSON string to convert.
     // @return The reconstructed StudyDeck object.
