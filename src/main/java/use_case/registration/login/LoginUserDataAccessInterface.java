4d066e437734068b09a642918ed8cc4fe0af68b9
Daniel Zhang <66927489+DanielZ818@users.noreply.github.com>
2025-11-29 16:46:08 -0500

    Change hashmap to map

--- src/main/java/use_case/registration/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/registration/login/LoginUserDataAccessInterface.java
@@ -1,12 +1,6 @@
-/**
- * File: LoginUserDataAccessInterface.java
- * Description: Defines the data access interface for login actions.
- * Author: Albert and Daniel
- */
-
 package use_case.registration.login;
 
-import java.util.HashMap;
+import java.util.Map;
 
 import use_case.DataAccessException;
 
@@ -14,16 +8,19 @@ import use_case.DataAccessException;
  * DAO interface for the Login Use Case.
  */
 public interface LoginUserDataAccessInterface {
+
     /**
-     * Return login status and user information.
+     * Returns login status and user information.
+     *
      * @param username the username of the account
      * @param password the password of the account
      * @return map of status and user info
-     * Example:
-     * login successful: {"status": true, "status_message": "", "user": User, "apiKey": "my_key"}
-     * Username not found: {"status": false, "status_message": "username dne"}
-     * Invalid Credential: {"status": false, "status_message": "invalid cred"}
-     * Unknown error: {"status": false, "status_message": "other status message"}
+     *         Example:
+     *         login successful: {"status": true, "status_message": "", "user": User, "apiKey": "my_key"}
+     *         Username not found: {"status": false, "status_message": "username dne"}
+     *         Invalid Credential: {"status": false, "status_message": "invalid cred"}
+     *         Unknown error: {"status": false, "status_message": "other status message"}
+     * @throws DataAccessException if there is an error accessing login data
      */
-    HashMap<String, Object> login(String username, String password) throws DataAccessException;
+    Map<String, Object> login(String username, String password) throws DataAccessException;
 }
